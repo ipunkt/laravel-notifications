5189fc1b182cc9a549711ee47e2e27c65cbc05b2
rokde <github@mail.robert-kummer.de>
2014-06-07 09:57:30 +0200

    sub view including corrected

--- src/views/bootstrap-3/flash.blade.php
+++ src/views/bootstrap-3/flash.blade.php
@@ -1,6 +1,6 @@
 @if (Session::has('flash_notification') && !empty(Session::get('flash_notification')))
 	@if (Session::has('flash_notification.overlay'))
-		@include('bootstrap-3/modal', ['modalClass' => 'flash-modal', 'title' => Lang::get('laravel-notifications::notifications.modal.title'), 'body' => Session::get('flash_notification.message')])
+		@include('ipunkt/laravel-notifications::bootstrap-3/modal', ['modalClass' => 'flash-modal', 'title' => Lang::get('laravel-notifications::notifications.modal.title'), 'body' => Session::get('flash_notification.message')])
 	@else
 		<div class="alert alert-{{ Session::get('flash_notification.level') }}">
 			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
