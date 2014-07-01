4cf42b1919ed7bd1a9de0658e89f684b94add0ab
Bastian Rang <justb81@users.noreply.github.com>
2014-07-01 11:12:48 +0200

    Add alert-dismissable class
    see http://getbootstrap.com/components/#alerts-dismissible

--- src/views/bootstrap-3/flash.blade.php
+++ src/views/bootstrap-3/flash.blade.php
@@ -2,7 +2,7 @@
 	@if (Session::has('flash_notification.overlay'))
 		@include('ipunkt/laravel-notifications::bootstrap-3/modal', ['modalClass' => 'flash-modal', 'title' => Lang::get('laravel-notifications::notifications.modal.title'), 'body' => Session::get('flash_notification.message')])
 	@else
-		<div class="alert alert-{{ Session::get('flash_notification.level') }}">
+		<div class="alert alert-dismissable alert-{{ Session::get('flash_notification.level') }}">
 			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 			<h4>{{ Session::get('flash_notification.message') }}</h4>
 		</div>
