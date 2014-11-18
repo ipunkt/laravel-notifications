126eca912f11c83803a941141d888cb9b64614d6
Robert Kummer <github@mail.robert-kummer.de>
2014-11-18 18:38:59 +0100

    never display on ViewErrorBags

--- src/views/bootstrap-3/message.blade.php
+++ src/views/bootstrap-3/message.blade.php
@@ -1,4 +1,12 @@
-<div class="alert alert-{{ $level }}">
+<?php
+
+//	a view error bag is for form validation errors, no our business at all
+//	-> these should be displayed at the field
+if ($message instanceof Illuminate\Support\ViewErrorBag) {
+	return;
+}
+
+?><div class="alert alert-{{ $level }}">
 	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 	<h5>{{ $message }}</h5>
 	@if (isset($body) && !empty($body)){{ $body }}@endif
